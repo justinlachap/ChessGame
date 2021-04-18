9cb9f579dfe198e1009946414a852bdd73dea219
justinlachap <justinlachapel@gmail.com>
2021-04-17 21:25:20 -0400

    position initale dune partie

--- CalcWindow.cpp
+++ CalcWindow.cpp
@@ -40,40 +40,16 @@ ChessWindow::ChessWindow(QWidget* parent) :
 	// Nom de la fenêtre
 	setWindowTitle("Jeu d'échecs");
 
-
-	// Initialisation des composantes du jeu
+	// Initalisation de l'échiquier
+	QLabel* lbl1 = new QLabel(this);
 	Echiquier e;
 	e.initialiserVide();
-	Dame d1(e, std::make_pair(0, 0), false);
-	Cavalier c1(e, std::make_pair(2, 2), true);
-
-	// Initialisation des labels en fonction du nombre de composantes
-	QLabel* lbl1 = new QLabel(this);
-	QLabel* lbl2 = new QLabel(this);
-	QLabel* lbl3 = new QLabel(this);
-	
-
-	// Initalisation des images
-	QString echiquier1 = e.obtenirImage();
-	QString reineNoire = d1.obtenirImage();
-	QString cavalierBlanc = c1.obtenirImage();
 	QPixmap echiquier;
-	QPixmap pix2;
-	QPixmap pix3;
-	echiquier.load(echiquier1);
-	pix2.load(reineNoire);
-	pix3.load(cavalierBlanc);
-
-	// Initalisation des positions sur l'échiquier
+	echiquier.load(e.obtenirImage());
 	lbl1->setMinimumSize(800, 800); // échiquier en soi
 	lbl1->setPixmap(echiquier.scaled(lbl1->width(), lbl1->height(), Qt::KeepAspectRatio));
 
-	lbl2->setMinimumSize(100, 100);
-	lbl2->setPixmap(pix2.scaled(lbl2->width(), lbl2->height(), Qt::KeepAspectRatio));
-	lbl2->setGeometry(QRect(100*d1.obtenirPosition().first, 100*(7-d1.obtenirPosition().second), 100, 100)); // (Qrect(x,y,height,width)
-	
-	lbl3->setMinimumSize(100, 100);
-	lbl3->setPixmap(pix3.scaled(lbl3->width(), lbl3->height(), Qt::KeepAspectRatio));
-	lbl3->setGeometry(QRect(100 * c1.obtenirPosition().first, 100*(7 - c1.obtenirPosition().second), 100, 100)); // (Qrect(x,y,height,width)
+	// Initialisation des pièces
+	positionInitiale(e);
 
 }
--- CalcWindow.hpp
+++ CalcWindow.hpp
@@ -8,7 +8,7 @@
 #include <QPushButton>
 #include <QString>
 #include <QLabel>
-
+#include "classes.hpp"
 #pragma pop()
 
 class Case;
@@ -24,7 +24,85 @@ public:
 	ChessWindow(QWidget* parent = nullptr);
 	~ChessWindow() override = default;
 	QList<Case*> cases;
+	void ajouterPiece(Piece* p) {
+		QLabel* lbl4 = new QLabel(this);
+		QString qstring = p->obtenirImage();
+		QPixmap pix4;
+		pix4.load(qstring);
 
+		lbl4->setMinimumSize(100, 100);
+		lbl4->setPixmap(pix4.scaled(lbl4->width(), lbl4->height(), Qt::KeepAspectRatio));
+		lbl4->setGeometry(QRect(100 * p->obtenirPosition().first, 100 * (7 - p->obtenirPosition().second), 100, 100)); // (Qrect(x,y,height,width)
+	}
+	void positionInitiale(Echiquier e) {
+		Roi R1(e, std::make_pair(4, 0), true);
+		Roi R2(e, std::make_pair(4, 7), false);
+		Dame d1(e, std::make_pair(3, 0), true);
+		Dame d2(e, std::make_pair(3, 7), false);
+		Pion P1(e, std::make_pair(0, 1), true);
+		Pion P2(e, std::make_pair(1, 1), true);
+		Pion P3(e, std::make_pair(2, 1), true);
+		Pion P4(e, std::make_pair(3, 1), true);
+		Pion P5(e, std::make_pair(4, 1), true);
+		Pion P6(e, std::make_pair(5, 1), true);
+		Pion P7(e, std::make_pair(6, 1), true);
+		Pion P8(e, std::make_pair(7, 1), true);
+		Pion P9(e, std::make_pair(0, 6), false);
+		Pion P10(e, std::make_pair(1, 6), false);
+		Pion P11(e, std::make_pair(2, 6), false);
+		Pion P12(e, std::make_pair(3, 6), false);
+		Pion P13(e, std::make_pair(4, 6), false);
+		Pion P14(e, std::make_pair(5, 6), false);
+		Pion P15(e, std::make_pair(6, 6), false);
+		Pion P16(e, std::make_pair(7, 6), false);
+		Cavalier c1(e, std::make_pair(1, 0), true);
+		Cavalier c2(e, std::make_pair(6, 0), true);
+		Cavalier c3(e, std::make_pair(1, 7), false);
+		Cavalier c4(e, std::make_pair(6, 7), false);
+		Tour r1(e, std::make_pair(0, 0), true);
+		Tour r2(e, std::make_pair(7, 0), true);
+		Tour r3(e, std::make_pair(0, 7), false);
+		Tour r4(e, std::make_pair(7, 7), false);
+		Fou f1(e, std::make_pair(2, 0), true);
+		Fou f2(e, std::make_pair(5, 0), true);
+		Fou f3(e, std::make_pair(2, 7), false);
+		Fou f4(e, std::make_pair(5, 7), false);
+
+
+
+		ajouterPiece(&d1);
+		ajouterPiece(&d2);
+		ajouterPiece(&R1);
+		ajouterPiece(&R2);
+		ajouterPiece(&P1);
+		ajouterPiece(&P2);
+		ajouterPiece(&P3);
+		ajouterPiece(&P4);
+		ajouterPiece(&P5);
+		ajouterPiece(&P6);
+		ajouterPiece(&P7);
+		ajouterPiece(&P8);
+		ajouterPiece(&P9);
+		ajouterPiece(&P10);
+		ajouterPiece(&P11);
+		ajouterPiece(&P12);
+		ajouterPiece(&P13);
+		ajouterPiece(&P14);
+		ajouterPiece(&P15);
+		ajouterPiece(&P16);
+		ajouterPiece(&c1);
+		ajouterPiece(&c2);
+		ajouterPiece(&c3);
+		ajouterPiece(&c4);
+		ajouterPiece(&r1);
+		ajouterPiece(&r2);
+		ajouterPiece(&r3);
+		ajouterPiece(&r4);
+		ajouterPiece(&f1);
+		ajouterPiece(&f2);
+		ajouterPiece(&f3);
+		ajouterPiece(&f4);
+	}
 
 
 private:
